4fa9e609196af4bfcd33c1166d10fa6ab407b76c
Antonio Celestino <antonio.celestino@gmail.com>
2022-05-08 10:40:50 +0300

    Created header for session check and redirect if not logged updated redirect links

---
+++ App/TheSite/AccessPage.php
@@ -0,0 +1,16 @@
+<?php
+include "../Register/header.php";
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Sam's Used Cars</title>
+   </head>
+
+<?php
+echo "Test"
+?>
+
+<p> <a href="../Register/login.php" style="color: blue;">Access Page</a> </p>
+
+</html>
