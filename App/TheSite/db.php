60b9e74d042751bceb6a16f6d7714f1288300814
SikozuSarean <antonio.celestino@gmail.com>
2022-07-10 16:35:05 +0300

    updated log in method in db

--- App/TheSite/db.php
+++ App/TheSite/db.php
@@ -1,7 +1,7 @@
 <?php
+include "secu_data.php";
 
-
-$conn = new mysqli('localhost', 'cars', 'a' );
+$conn = new mysqli('localhost', $db_user_toni, $db_pwd_toni );
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
